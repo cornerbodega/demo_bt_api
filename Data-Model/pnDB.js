dbf4b88647a07e7bed61526a916f7a12a6526e10
Cornerbodega <merhone@gmail.com>
2017-09-26 16:22:42 -0700

    php

--- Data-Model/pnDB.js
+++ Data-Model/pnDB.js
@@ -26,42 +26,43 @@ function pnDB($http, pnProxy, $q, pnPost) {
 
     function saveToDB(table, data) {
         // console.log('getFromDB!');
-        return pnProxy('https://abctraceability.com/wa/Data-Model/php/saveToDB.php', {table:table, data: data});
+        return pnProxy('https://abctraceability.com/demo/demo_bt_api/Data-Model/php/saveToDB.php', {table:table, data: data});
     };;
     function getFromDB(query) {
         // console.log('getFromDB!');
-        return pnProxy('https://abctraceability.com/wa/Data-Model/php/getFromDB.php', {query: query});
+        // return pnProxy('https://abctraceability.com/demo/demo_bt_api/Data-Model/php/getFromDB.php', {query: query});
+        return pnProxy('https://abctraceability.com/demo/demo_bt_api/Data-Model/php/getFromDB.php', {query: query});
     };
     function saveManifestRequest(request) {
-        return pnProxy('https://abctraceability.com/wa/Data-Model/php/listings/saveManifestRequest.php', request);
+        return pnProxy('https://abctraceability.com/demo/demo_bt_api/Data-Model/php/listings/saveManifestRequest.php', request);
     };
     function getBusinesses() {
-        return pnProxy('https://abctraceability.com/wa/Data-Model/php/businesses/getBusinessesFromDB.php');
+        return pnProxy('https://abctraceability.com/demo/demo_bt_api/Data-Model/php/businesses/getBusinessesFromDB.php');
     };
     // function getMyBuyListings() {
-    //     return pnProxy('https://abctraceability.com/wa/Data-Model/php/listings/get_my_buy_listings.php', {ubi: sessionStorage.ubi});
+    //     return pnProxy('https://abctraceability.com/demo/demo_bt_api/Data-Model/php/listings/get_my_buy_listings.php', {ubi: sessionStorage.ubi});
     // };
     // function getMySellListings() {
-    //     return pnProxy('https://abctraceability.com/wa/Data-Model/php/listings/get_my_sell_listings.php', {ubi: sessionStorage.ubi})
+    //     return pnProxy('https://abctraceability.com/demo/demo_bt_api/Data-Model/php/listings/get_my_sell_listings.php', {ubi: sessionStorage.ubi})
     // };
     // function getAllBuyListings() {
-    //     return pnProxy('https://abctraceability.com/wa/Data-Model/php/listings/get_buy_listings.php');
+    //     return pnProxy('https://abctraceability.com/demo/demo_bt_api/Data-Model/php/listings/get_buy_listings.php');
     // };
     // function getAllSellListings() {
-    //     return pnProxy('https://abctraceability.com/wa/Data-Model/php/listings/get_sell_listings.php')
+    //     return pnProxy('https://abctraceability.com/demo/demo_bt_api/Data-Model/php/listings/get_sell_listings.php')
     // };
     // function saveWantToBuyListing(listing) {
     //     console.log(listing);
     //     if(!listing) return console.log('No WTB Listing!!');
-    //     return pnProxy('https://abctraceability.com/wa/Data-Model/php/listings/save_want_to_buy_listing.php', listing);
+    //     return pnProxy('https://abctraceability.com/demo/demo_bt_api/Data-Model/php/listings/save_want_to_buy_listing.php', listing);
     // };
     function saveWantToSellListings(listings) {
         if(!listings) return console.log('Error! NO LISTINGS!');
         console.log(listings);
-        // return pnProxy('https://abctraceability.com/wa/Data-Model/php/listings/save_listing.php', listing);
+        // return pnProxy('https://abctraceability.com/demo/demo_bt_api/Data-Model/php/listings/save_listing.php', listing);
         var listingPromises = _.map(listings, function(listing) {
-            // return pnProxy('https://abctraceability.com/wa/Data-Model/php/listings/save_want_to_sell_listing.php', listing);
-            return pnProxy('https://abctraceability.com/wa/Data-Model/php/saveToDB.php', {table: 'wts_listings', data: listing});
+            // return pnProxy('https://abctraceability.com/demo/demo_bt_api/Data-Model/php/listings/save_want_to_sell_listing.php', listing);
+            return pnProxy('https://abctraceability.com/demo/demo_bt_api/Data-Model/php/saveToDB.php', {table: 'wts_listings', data: listing});
 
         })
         return $q.all(listingPromises);
@@ -74,7 +75,7 @@ function pnDB($http, pnProxy, $q, pnPost) {
         console.log(user);
         // console.log('SAVBE');
         if (!user) return console.log('Error! No user info. Bad news... Impossible!');
-        return pnProxy('https://abctraceability.com/wa/Data-Model/php/users/save_user.php', user);
+        return pnProxy('https://abctraceability.com/demo/demo_bt_api/Data-Model/php/users/save_user.php', user);
     };
 
     function getUserByID(user_data) {
@@ -83,7 +84,7 @@ function pnDB($http, pnProxy, $q, pnPost) {
         if (!user_data) {
             return console.log('Error! user_data is not defined!');
         };
-        return pnProxy('https://abctraceability.com/wa/Data-Model/php/users/does_user_exist.php', {user_id: user_data.user_id})
+        return pnProxy('https://abctraceability.com/demo/demo_bt_api/Data-Model/php/users/does_user_exist.php', {user_id: user_data.user_id})
     };
 
     function getPos() {
